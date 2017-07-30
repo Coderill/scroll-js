d9f36ea5d60175bfb165468e60a3c4b35eababfa
Jayanta Biswas <bjayanta.me@gmail.com>
2017-07-30 11:57:53 +0600

    work with auto selection and scroll

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-var env = $('#scroll-env');
+// set the environment
+var env 				= $('#scroll-env'),
+	screenPercentage 	= 30,
+	duration 			= 800,
+	effect 				= 'easeInOutQuad';
 
+// make the menu item scrollable in click event
 env.on('click', 'a[href^="#"]', function(event) {
-	var anchor 		= $(this),
-		target 		= $($(this).attr('href')),
-		duration 	= 800,
-		effect 		= 'easeInOutQuad';
+	var anchor 			= $(this),
+		target 			= $($(this).attr('href'));
 
+	// check the target link exists or not
 	if( target.length ) {
 		event.preventDefault();
 
+		// set the section scrollable with animation
 		$('html, body').animate({
 			scrollTop: (target.offset().top - env.outerHeight())
 		}, duration, effect, function() {
@@ -16,4 +21,21 @@ env.on('click', 'a[href^="#"]', function(event) {
 			anchor.addClass('active');
 		});
 	}
+});
+
+// select the anchor using the scroll
+$(document).scroll(function() {
+	var screenSize 			= $(window).height() * (screenPercentage / 100),
+		scrollPosition 		= $(document).scrollTop() + screenSize;
+
+	$("#scroll-env a").each(function() {
+		var self 			= $(this),
+			targetID 		= $(self.attr("href")),
+			elementPosition = targetID.offset().top;
+
+		if (targetID.position().top <= scrollPosition && (targetID.position().top + targetID.height()) > scrollPosition) {
+           $("#scroll-env a").removeClass("active");
+           self.addClass("active");
+        }
+	});
 });
